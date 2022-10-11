f5cf09ab7b2fbdf20417901bc5446155c4b85327
TouHunp <mmilove3755@gmail.com>
2022-10-12 02:27:45 +0800

    training

--- promise.js
+++ promise.js
@@ -6,16 +6,17 @@ let p = new Promise(function (resolve, reject) {
     reject("form reject(): order cancelled");
   }
 });
-
+//message 用來接收resolve或reject 觸發回傳的值
 p.then(function (message) {
   console.log(`${message}-promise resolved`);
 }).catch(function (message) {
   console.log(`${message}-promise rejected`);
 });
-
+// fetch 可以獲取數據 建立promise
 let f = fetch("https://jsonplaceholder.typicode.com/users");
 
 f.then(function (userdata) {
+  // 將獲取數據轉換成json格式 再次建立promise 所以讀取需要再加一個.then
   return userdata.json();
 }).then(function (jsondata) {
   console.log(jsondata);
