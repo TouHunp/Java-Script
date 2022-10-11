2c3242380c15326e2a8d3a3c8f24a645afeaf477
TouHunp <mmilove3755@gmail.com>
2022-10-12 02:55:51 +0800

    training

--- async-await.js
+++ async-await.js
@@ -1,15 +1,21 @@
 function sendRequest() {
   return new Promise(function (resolve, reject) {
     setTimeout(function () {
-      resolve("John Doe");
+      reject("Request rejected");
     }, 2000);
   });
 }
 // 非同步要在function前加async
 // 配合await,await會等sendRequest()返回Promise,才會將值傳給username
+/* 在async function裡使用try{}嘗試執行錯誤代碼 而catch(message){}來捕捉reject錯誤訊息,
+reject返回的訊息會在 message*/
 async function getUsername() {
-  let username = await sendRequest();
-  console.log(username);
+  try {
+    let username = await sendRequest();
+    console.log(username);
+  } catch (message) {
+    console.log(`Error: ${message}`);
+  }
 }
 
 getUsername();
