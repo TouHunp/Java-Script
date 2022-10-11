4833f36c803a0ffb3db815c99569d41905d85de6
TouHunp <mmilove3755@gmail.com>
2022-10-12 02:25:23 +0800

    training

--- promise.js
+++ promise.js
@@ -1,4 +1,4 @@
-let sentToAirport = true;
+let sentToAirport = false;
 let p = new Promise(function (resolve, reject) {
   if (sentToAirport) {
     resolve("from resolve(): send to airport");
@@ -11,4 +11,12 @@ p.then(function (message) {
   console.log(`${message}-promise resolved`);
 }).catch(function (message) {
   console.log(`${message}-promise rejected`);
+});
+
+let f = fetch("https://jsonplaceholder.typicode.com/users");
+
+f.then(function (userdata) {
+  return userdata.json();
+}).then(function (jsondata) {
+  console.log(jsondata);
 });
